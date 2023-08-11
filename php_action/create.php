3157fee0e328a61093ee5704976397734548ab49
Ívano Fontes <ivanofnts@gmail.com>
2023-08-11 12:03:07 -0300

    prevenção xss

--- php_action/create.php
+++ php_action/create.php
@@ -2,16 +2,23 @@
 session_start();
 require_once 'db_connect.php';
 
+function clear($input) {
+  global $connect;
+  $var = mysqli_escape_string($connect, $input);
+  $var = htmlspecialchars($var);
+  return $var;
+}
+
 if (isset($_POST['btn-cadastrar'])) {
 
 	if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)) {
 		$_SESSION['mensagem'] = "Formato de idade inválido! Use números!";
 		header('Location: ../adicionar.php');
 	} else { 
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
   
   $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
   
